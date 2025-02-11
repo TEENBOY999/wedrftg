e51616a401967efc027cc2e61f011643e4021ebd
TEENBOY999 <ABDURAHMON12890@gmail.com>
2025-02-11 12:16:52 +0500

    Add files via upload

---
+++ spp.js
@@ -0,0 +1,18 @@
+// let myName = prompt("kak vas zavut")
+// let myAge = prompt("kakoy uvas vozrast")
+// let student = true
+// const myDocs = "Tom"
+
+let a = prompt("a tamonni kiriting:")
+let b = prompt("b tomonni kiriting:")
+let c = (Number (a) + Number (b))*2
+let d = (Number (a) * Number (b))
+
+alert(`a tomon ${a}  b tomon ${b} P ${c}  S ${d} `)
+
+// alert(` sizning ismingiz   ${myName}  sizning yoshingiz  ${myAge} `)
+//if(student){
+    //alert("PDP Xush keldingiz!!!")
+//}else{
+    ///alert("PDP ga o`qishga kiring")
+//} 
